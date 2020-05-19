88c797928a10e30d3c8cc1fcbe631652ee91d7d6
jamesnitz <jamesanitz2@gmail.com>
2020-05-19 10:31:54 -0500

    functions and methods

--- debugg.py
+++ debugg.py
@@ -1,7 +1,14 @@
-msg = "Hello World"
+msg = "Hello Worldf"
 
-# practicce writing if else
+# practice writing if else
 if msg == "Hello World":
   print(msg)
 else:
   print("I don't know the message")
+
+# I made a function
+def friendlyMsg(name):
+  return f'Hello, {name}! Have a great day!'
+
+print(friendlyMsg("james").upper())
+print(friendlyMsg("clayton"))
